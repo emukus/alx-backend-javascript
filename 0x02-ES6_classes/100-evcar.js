a8bb23827ad0126d6955bf66ad7d48e420b6b68b
emukus <mukuna.eugene@gmail.com>
2023-01-26 21:39:33 +0000

    correction

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,17 +1,14 @@
 import Car from './10-car';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    // call constructor of super class (Building)
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
-
-    // Create objs
     this._range = range;
   }
-
-  // Methods
 
   static get [Symbol.species]() {
     return Car;
   }
 }
+
+export default EVCar;
