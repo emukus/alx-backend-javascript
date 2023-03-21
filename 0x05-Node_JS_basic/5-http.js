7a71bc64694d2ddf4947648993fa2a55dce19e9d
emukus <101443265+emukus@users.noreply.github.com>
2023-03-21 22:18:32 +0000

    Node JS Basics

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,37 +1,20 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
-
-const hostname = '127.0.0.1';
-const port = 1245;
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  const { url } = req;
-
-  if (url === '/') {
-    res.write('Hello Holberton School!');
-  } else if (url === '/students') {
+  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
   }
-  res.statusCode = 404;
   res.end();
 });
-
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
-
-});
-
+app.listen(1245);
 module.exports = app;
